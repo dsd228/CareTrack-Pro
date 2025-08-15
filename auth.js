f617f597a8ea4bcf518d0197aed2126f3f600318
Diaz David sebastian <dsd228.dd@ies21.onmicrosoft.com>
2025-08-15 16:06:11 -0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,21 +1,12 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
-import { getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut, createUserWithEmailAndPassword, updateProfile } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
-import { getFirestore, doc, getDoc, setDoc } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
-
-// Configuración de tu proyecto Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyBW0QcJ5jCCx52l9qfYPTj5lUHp1TjlSpA",
-  authDomain: "caretrackweb.firebaseapp.com",
-  projectId: "caretrackweb",
-  storageBucket: "caretrackweb.appspot.com",
-  messagingSenderId: "31200607142",
-  appId: "1:31200607142:web:c4902c1a4a1b30d962ad67",
-  measurementId: "G-0G8GHGHF7Y"
-};
-
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+import { auth, db } from './firebase.js';
+import {
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+  createUserWithEmailAndPassword,
+  updateProfile
+} from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
+import { doc, getDoc, setDoc } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
 
 let currentUser = null;
 let role = "medico";
@@ -43,14 +34,9 @@ export function initAuth() {
 export function login(email, pass) {
   return signInWithEmailAndPassword(auth, email, pass);
 }
-
 export function logout() { return signOut(auth); }
-
 export function getRole(user=currentUser) { return role; }
-
-export function onUserChange(cb) {
-  onAuthStateChanged(auth, cb);
-}
+export function onUserChange(cb) { onAuthStateChanged(auth, cb); }
 
 export async function register(email, pass, name, roleUser="medico") {
   const cred = await createUserWithEmailAndPassword(auth, email, pass);
